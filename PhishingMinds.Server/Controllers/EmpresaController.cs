16cb5419261c312c1f10633ae4bf07c629f8da25
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2026-03-25 11:47:03 +0000

    Update: controller e class Empresa

--- PhishingMinds.Server/Controllers/EmpresaController.cs
+++ PhishingMinds.Server/Controllers/EmpresaController.cs
@@ -7,20 +7,18 @@ namespace PhishingMinds.Server.Controllers
     [ApiController]
     public class EmpresaController : ControllerBase
     {
-        private static List<Empresa> empresas = new List<Empresa>();
+        private static List<Empresa> empresas = new List<Empresa>(); //implementar o banco dps nessas listas (setor tb)
 
-        // GET: api/empresa
         [HttpGet]
         public ActionResult<List<Empresa>> Get()
         {
             return Ok(empresas);
         }
 
-        // GET: api/empresa/id da empresa
         [HttpGet("{id}")]
         public ActionResult<Empresa> GetById(int id)
         {
-            var empresa = empresas.FirstOrDefault(e => e.IdPlano == id);
+            var empresa = empresas.FirstOrDefault(e => e.IdEmpresa == id);
 
             if (empresa == null)
                 return NotFound("Empresa não encontrada");
@@ -28,23 +26,26 @@ namespace PhishingMinds.Server.Controllers
             return Ok(empresa);
         }
 
-        // POST: api/empresa
         [HttpPost]
         public ActionResult Create(Empresa novaEmpresa)
         {
+            novaEmpresa.IdEmpresa = empresas.Count > 0 ? empresas.Max(e => e.IdEmpresa) + 1 : 1;
+            novaEmpresa.Dt_Cadastro = DateTime.Now;
+
             empresas.Add(novaEmpresa);
-            return Ok("Empresa criada com sucesso");
+
+            return Ok(novaEmpresa);
         }
 
-        // PUT: api/empresa/id da empresa
         [HttpPut("{id}")]
         public ActionResult Update(int id, Empresa empresaAtualizada)
         {
-            var empresa = empresas.FirstOrDefault(e => e.IdPlano == id);
+            var empresa = empresas.FirstOrDefault(e => e.IdEmpresa == id);
 
             if (empresa == null)
                 return NotFound("Empresa não encontrada");
 
+            empresa.IdPlano = empresaAtualizada.IdPlano;
             empresa.Nm_Empresa = empresaAtualizada.Nm_Empresa;
             empresa.Nm_Dono = empresaAtualizada.Nm_Dono;
             empresa.Mail = empresaAtualizada.Mail;
@@ -53,14 +54,13 @@ namespace PhishingMinds.Server.Controllers
             empresa.Dt_FimContrato = empresaAtualizada.Dt_FimContrato;
             empresa.Ativo = empresaAtualizada.Ativo;
 
-            return Ok("Empresa atualizada");
+            return Ok(empresa);
         }
 
-        // DELETE: api/empresa/id da empresa
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)
         {
-            var empresa = empresas.FirstOrDefault(e => e.IdPlano == id);
+            var empresa = empresas.FirstOrDefault(e => e.IdEmpresa == id);
 
             if (empresa == null)
                 return NotFound("Empresa não encontrada");
