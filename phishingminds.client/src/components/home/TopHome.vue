13193f31399b8a07e12d0ca3a7343e598d7b78ed
gustavo-delinski <169003880+Gustavo-Delinski@users.noreply.github.com>
2026-04-28 16:38:45 -0300

    Pagina Home

---
+++ phishingminds.client/src/components/home/TopHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-6 pt-2">
+
+    <div class="flex justify-between items-center mb-5">
+      <input
+        type="text"
+        placeholder="Buscar..."
+        class="bg-white px-5 py-3 rounded-xl shadow-sm outline-none w-72"
+      />
+
+      <div class="flex items-center gap-3">
+        
+
+        <div class="text-right leading-tight">
+          <h4 class="font-semibold text-sm text-gray-700">Administrador</h4>
+          <p class="text-xs text-gray-400">Usuário</p>
+        </div>
+
+        <div class="w-10 h-10 bg-green-700 rounded-full"></div>
+      </div>
+    </div>
+
+    <h2 class="text-4xl font-bold text-green-900">Overview da Segurança</h2>
+    <p class="text-gray-500 mt-1">Monitoramento em tempo real de vulnerabilidades e métricas.</p>
+  </div>
+</template>
